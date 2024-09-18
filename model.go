0ec03009b0ca571ad6c9b43d45d882b4e18201cf
nuominmin <776191753@qq.com>
2024-09-18 11:01:00 +0800

    chore: 减少 task_name 长度

--- model.go
+++ model.go
@@ -6,10 +6,10 @@ import (
 
 type WaterLevel struct {
 	ID         int64     `gorm:"<-:create;column:id;primaryKey;autoIncrement"`
-	TaskName   string    `gorm:"<-:create;column:task_name;type:varchar(255);index:idx_task_name;not null;comment:'任务的名称，用于区分不同的任务'"`
-	WaterLevel uint64    `gorm:"column:water_level;type:bigint;not null;comment:'记录任务进度的水位线，其具体意义根据实际业务决定'"`
-	CreatedAt  time.Time `gorm:"column:created_at;autoCreateTime:milli;comment:'记录创建时间'"`
-	UpdatedAt  time.Time `gorm:"column:updated_at;autoUpdateTime:milli;comment:'记录最后一次更新时间'"`
+	TaskName   string    `gorm:"<-:create;column:task_name;type:varchar(200);index:idx_task_name;not null;comment:任务的名称，用于区分不同的任务"`
+	WaterLevel uint64    `gorm:"column:water_level;type:bigint;not null;comment:记录任务进度的水位线，其具体意义根据实际业务决定"`
+	CreatedAt  time.Time `gorm:"column:created_at;autoCreateTime:milli;comment:记录创建时间"`
+	UpdatedAt  time.Time `gorm:"column:updated_at;autoUpdateTime:milli;comment:记录最后一次更新时间"`
 }
 
 func (c WaterLevel) TableName() string {
